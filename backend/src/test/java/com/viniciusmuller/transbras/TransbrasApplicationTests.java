8bbe98b619f08c00af5b259c423e06907b482630
Muller <zandremuller@yahoo.com.br>
2026-01-29 13:42:08 -0300

    feat: add initial files

---
+++ backend/src/test/java/com/viniciusmuller/transbras/TransbrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusmuller.transbras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransbrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
