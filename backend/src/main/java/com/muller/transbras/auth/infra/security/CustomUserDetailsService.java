8092be2ca27617f94772c570676423a24f8fd147
Muller <zandremuller@yahoo.com.br>
2026-02-03 14:20:50 -0300

    feat: add CustomUserDetailsService

---
+++ backend/src/main/java/com/muller/transbras/auth/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.muller.transbras.auth.infra.security;
+
+import com.muller.transbras.auth.model.User;
+import com.muller.transbras.auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User: "+username+" not found in repository"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
