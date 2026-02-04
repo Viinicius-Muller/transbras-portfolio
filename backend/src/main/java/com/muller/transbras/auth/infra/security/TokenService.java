8387e1b118b2caca6fa89166e408529e40486add
Muller <zandremuller@yahoo.com.br>
2026-02-04 10:27:26 -0300

    feat: add TokenService

---
+++ backend/src/main/java/com/muller/transbras/auth/infra/security/TokenService.java
@@ -0,0 +1,53 @@
+package com.muller.transbras.auth.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.muller.transbras.auth.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${app.security.jwt.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        String token = JWT.create()
+                .withIssuer("transbras-auth-service")
+                .withSubject(user.getUsername())
+                .withClaim("userId", user.getId())
+                .withExpiresAt(this.generateExpiration())
+                .sign(algorithm);
+
+        return token;
+    } catch (JWTCreationException e) {
+        throw new RuntimeException("Failed to generate Token");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("transbras-auth-service")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+
+    public Instant generateExpiration() {
+        return LocalDateTime.now().plusDays(2).toInstant(ZoneOffset.of("-3:00"));
+    }
+}
