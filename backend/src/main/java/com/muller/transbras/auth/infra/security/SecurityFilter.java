53f16bbd1363b0724fc272c6c9181b985100eb1f
Muller <zandremuller@yahoo.com.br>
2026-02-04 10:43:34 -0300

    feat: add SecurityFilter, SecurityConfig

---
+++ backend/src/main/java/com/muller/transbras/auth/infra/security/SecurityFilter.java
@@ -0,0 +1,44 @@
+package com.muller.transbras.auth.infra.security;
+
+import com.muller.transbras.auth.model.User;
+import com.muller.transbras.auth.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        var login = tokenService.validateToken(token);
+
+        if (login != null) {
+            User user = userRepository.findByUsername(login)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+            var authentication = new UsernamePasswordAuthenticationToken(user, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
